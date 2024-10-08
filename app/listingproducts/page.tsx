4be0b7f65de8241fe713679ac5c827c2450dcb4e
Ehtesham86 <buttehtesham86@gmail.com>
2024-10-08 03:05:03 -0700

    2 12

--- app/listingproducts/page.tsx
+++ app/listingproducts/page.tsx
@@ -23,7 +23,7 @@ const Modal = () => {
     setloading(true);
     try {
       const response = await fetch('https://amazon-api-five.vercel.app/api/products', {
-        mode: 'cors', // Add this to handle cross-origin requests
+        mode: 'no-cors', // Add this to handle cross-origin requests
       });
   
       if (!response.ok) {
